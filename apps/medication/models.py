105abbf5482522a85760d9d4341ea46680b98cb3
Omonjon2004 <inatovomonjon8@gmail.com>
2024-11-30 16:27:44 +0500

    push_2

---
+++ apps/medication/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from apps.shared.models import TimeStampedModel
+
+
+# Create your models here.
+
+
+class Medications(TimeStampedModel):
+    name = models.CharField()
+    description = models.TextField()
+    DOSAGE_FORM_CHOICES = (
+        ('SYRUP','Syrup'),
+        ('CREAM','Cream'),
+        ('TABLET','Tablet'),
+        ('CAPSULE','Capsule'),
+    )
+    dosage_form=models.CharField(choices=DOSAGE_FORM_CHOICES, max_length=20)
+    strength=models.CharField(max_length=20)
+    price=models.FloatField()
+    stock_quantity=models.IntegerField()
+    manufacturer=models.CharField(max_length=255)
+
