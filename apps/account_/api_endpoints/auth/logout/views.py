c759ddd2e638d267cb42a088e30136c2281bebb0
Omonjon2004 <inatovomonjon8@gmail.com>
2024-12-08 17:39:02 +0500

    push_11

---
+++ apps/account_/api_endpoints/auth/logout/views.py
@@ -0,0 +1,20 @@
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data.get("refresh")
+            if not refresh_token:
+                return Response({"error": "Refresh token is required."}, status=400)
+
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response({"message": "Successfully logged out."}, status=200)
+        except Exception as e:
+            return Response({"error": str(e)}, status=400)
--- apps/account_/urls/v1.py
+++ apps/account_/urls/v1.py
@@ -2,11 +2,13 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from apps.account_.api_endpoints import auth
+from apps.account_.api_endpoints.auth.logout import LogoutView
 from apps.account_.api_endpoints.social_auth import FacebookLogin, GoogleLogin, AppleLogin
 
 urlpatterns = [
     path('login/', auth.LoginView.as_view(), name='login'),
     path('register/', auth.RegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('facebook/', FacebookLogin.as_view(), name='fb_login'),
     path('google/', GoogleLogin.as_view(), name='google_login'),
     path('apple/login/', AppleLogin.as_view(), name='apple-login'),
