a5a4f1ddc115efa2710910f4f033b741e902ce6e
Omonjon2004 <inatovomonjon8@gmail.com>
2024-12-16 20:15:50 +0500

    create basket admin

--- apps/basket/admin.py
+++ apps/basket/admin.py
@@ -3,33 +3,26 @@ from django.contrib import admin
 from apps.basket.models import Basket, BasketItem
 
 
-# @admin.register(Basket)
-# class BasketAdmin(admin.ModelAdmin):
-#     search_fields = (
-#         "id",
-#         "user__full_name",
-#         "created_at",
-#         "updated_at"
-#     )
-#     list_display = (
-#         "id",
-#         "user",
-#         "created_at",
-#         "updated_at"
-#     )
-#
-#
-# @admin.register(BasketItem)
-# class BasketItemAdmin(admin.ModelAdmin):
-#     search_fields = (
-#         "id",
-#         "medication__name",
-#         "quantity",
-#         "added_at"
-#     )
-#     list_display = (
-#         "id",
-#         "medication",
-#         "quantity",
-#         "added_at"
-#     )
+@admin.register(Basket)
+class BasketAdmin(admin.ModelAdmin):
+    search_fields = (
+        "id",
+        "user__full_name",
+        "created_at",
+        "updated_at"
+    )
+    list_display = (
+        "id",
+        "user",
+        "created_at",
+        "updated_at"
+    )
+
+
+@admin.register(BasketItem)
+class BasketItemAdmin(admin.ModelAdmin):
+    search_fields = (
+        'basket',
+        'medication',
+        'quantity'
+    )
